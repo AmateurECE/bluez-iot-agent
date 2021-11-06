e0c7bc2fc2d37b633788a601c772977b0b4c2c1a
Ethan D. Twardy <ethan.twardy@gmail.com>
2021-11-06 14:26:42 -0500

    Integrate libmicrohttpd and write a quick example application

--- source/bluez-soundsystem-agent.c
+++ source/bluez-soundsystem-agent.c
@@ -7,7 +7,7 @@
 //
 // CREATED:         11/05/2021
 //
-// LAST EDITED:     11/05/2021
+// LAST EDITED:     11/06/2021
 //
 // Copyright 2021, Ethan D. Twardy
 //
@@ -25,10 +25,39 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ////
 
+#include <sys/types.h>
+#include <sys/select.h>
+#include <sys/socket.h>
+#include <microhttpd.h>
 #include <stdio.h>
+#include <string.h>
+
+static const int PORT = 8888;
+
+enum MHD_Result answer_to_connection(void* cls,
+    struct MHD_Connection* connection, const char* url, const char* method,
+    const char* version, const char* upload_data, size_t* upload_data_size,
+    void** con_cls)
+{
+    const char* page = "<html><body>Hello, browser!</body></html>";
+    struct MHD_Response* response = MHD_create_response_from_buffer(
+        strlen(page), (void*)page, MHD_RESPMEM_PERSISTENT);
+
+    int result = MHD_queue_response(connection, MHD_HTTP_OK, response);
+    MHD_destroy_response(response);
+    return result;
+}
 
 int main() {
-    printf("Hello, world!\n");
+    struct MHD_Daemon* daemon = MHD_start_daemon(
+        MHD_USE_INTERNAL_POLLING_THREAD, PORT, NULL, NULL,
+        &answer_to_connection, NULL, MHD_OPTION_END);
+    if (NULL == daemon) {
+        return 1;
+    }
+
+    getchar();
+    MHD_stop_daemon(daemon);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
