8c271af6cca97cf0499f3b04d5b1c23d3c298da1
Ethan D. Twardy <ethan.twardy@gmail.com>
2021-11-20 03:50:20 -0600

    Re-implement project infrastructure

---
+++ source/bluez-iot-agent.c
@@ -0,0 +1,54 @@
+///////////////////////////////////////////////////////////////////////////////
+// NAME:            bluez-iot-agent.c
+//
+// AUTHOR:          Ethan D. Twardy <ethan.twardy@gmail.com>
+//
+// DESCRIPTION:     Entrypoint for the application
+//
+// CREATED:         11/20/2021
+//
+// LAST EDITED:     11/20/2021
+//
+// Copyright 2021, Ethan D. Twardy
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+////
+
+#include <argp.h>
+
+#include <config.h>
+
+const char* argp_program_name = PROGRAM_NAME " " PROGRAM_VERSION;
+const char* argp_program_bug_address = "<ethan.twardy@gmail.com>";
+
+static char doc[] = "Modern pairing wizard for Bluetooth IoT devices on Linux";
+
+static error_t parse_opt(int, char*, struct argp_state*);
+static struct argp argp = { NULL, parse_opt, NULL, doc, NULL, NULL, NULL };
+
+static error_t parse_opt(int key, char* arg, struct argp_state* state) {
+    switch (key) {
+    case ARGP_KEY_END:
+        return 0;
+    default:
+        return ARGP_ERR_UNKNOWN;
+    }
+}
+
+int main(int argc, char** argv) {
+    argp_parse(&argp, argc, argv, 0, 0, NULL);
+    return 0;
+}
+
+///////////////////////////////////////////////////////////////////////////////
